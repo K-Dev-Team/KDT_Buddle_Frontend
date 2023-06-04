6378d42f0a01ffd8b95758f99deaee315d3d6038
lyn94 <lyn_94@naver.com>
2023-06-05 01:01:26 +0900

    chore: zustand 설치 및 라우팅, 스타일링 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import TestPage from './components/pages/TestPage';
+import Navigation from './components/templates/Navigation';
 
 const App: React.FC = () => {
   return (
       <div className="App">
-        Hello Typescript!
+        <BrowserRouter>
+          Hello Typescript!
+          <Navigation /> 
+          <Routes>
+            <Route path='/testPage' element={<TestPage/>}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
   );
 }
