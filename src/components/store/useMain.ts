e98785ff4cbc5e0d015d70fabc9ef2c0fffeb0be
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2023-06-06 21:21:53 +0900

    feature/TestCompo
    test zustand store

---
+++ src/components/store/useMain.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface Store {
+  state: number;
+  increase: () => void;
+  remove: () => void;
+}
+
+const useBearStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state: any) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}));
+
+export const useTests = () => {
+  const bears = useBearStore((state: any) => {
+    return {
+      state: state.bears,
+      increase: state.increasePopulation,
+      remove: state.removeAllBears,
+    };
+  });
+
+  return {
+    bears,
+  };
+};
