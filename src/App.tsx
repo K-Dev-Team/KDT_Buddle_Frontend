95ca6d7a6d0b1c7bd32902cd2b241b08c8081356
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2023-06-06 21:21:53 +0900

    feature/TestCompo
    test zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import TestPage from './components/pages/TestPage';
-import Navigation from './components/templates/Navigation';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import TestPage from "./components/pages/TestPage";
+import Navigation from "./components/templates/Navigation";
+import { useTests } from "./components/store/useMain";
 
 const App: React.FC = () => {
+  const useTest: any = useTests();
   return (
-      <div className="App">
-        <BrowserRouter>
-          Hello Typescript!
-          <Navigation /> 
-          <Routes>
-            <Route path='/testPage' element={<TestPage/>}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        Hello Typescript!
+        <div>
+          Store Test
+          {useTest.bears.state}
+          <button onClick={useTest.bears.increase}>plus</button>
+          <button onClick={useTest.bears.remove}>remove</button>
+        </div>
+        <Navigation />
+        <Routes>
+          <Route path="/testPage" element={<TestPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
