74d5ac5580e0683e78a0535bd40b9e5d79d7738c
lyn94 <lyn_94@naver.com>
2023-07-29 19:30:03 +0900

    [YNL] style: 전체 레이아웃 뷰 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import TestBear from './components/pages/TestBear';
-
-import Navigation from './components/templates/Navigation';
 import TestButton from './components/pages/TestButton';
 import TestMobx from './components/pages/TestMobx';
 import TestRedux from './components/pages/TestRedux';
+import Navigation from './components/templates/Navigation';
+import AppLayout from './styles/Layout';
+import LayoutBox from './styles/LayoutBox';
 
 const App: React.FC = () => (
   <div className="App">
-    <BrowserRouter>
+    <AppLayout>
+      <LayoutBox>
+        <BrowserRouter>
       Hello Typescript!
-      <Navigation />
-      <Routes>
-        <Route path="/testBear" element={<TestBear />} />
-        <Route path="/testButton" element={<TestButton />} />
-        <Route path="/testMobx" element={<TestMobx />} />
-        <Route path="/testRedux" element={<TestRedux />} />
-      </Routes>
-    </BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/testBear" element={<TestBear />} />
+            <Route path="/testButton" element={<TestButton />} />
+            <Route path="/testMobx" element={<TestMobx />} />
+            <Route path="/testRedux" element={<TestRedux />} />
+          </Routes>
+        </BrowserRouter>
+      </LayoutBox>
+    </AppLayout>
   </div>
 );
 
