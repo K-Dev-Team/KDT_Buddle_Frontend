aabeb672e5816cc1f80827a2d43d35c46206cfe9
lyn94 <lyn_94@naver.com>
2023-08-20 16:19:23 +0900

    [YNL] feat: styled component theme 설정 및 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import HomeButton from './components/common/HomeButton';
 import HomeHeader from './components/home/HomeHeader';
@@ -9,21 +10,26 @@ import TestButton from './components/pages/TestButton';
 import TestMobx from './components/pages/TestMobx';
 import TestRedux from './components/pages/TestRedux';
 import Navigation from './components/templates/Navigation';
+import GlobalStyles from './styles/GlobalStyle';
 import AppLayout from './styles/Layout';
 import LayoutBox from './styles/LayoutBox';
+import theme from './styles/Theme';
 
 const App: React.FC = () => (
   <div className="App">
-    <AppLayout>
-      <LayoutBox>
-        <BrowserRouter>
-          <HomeHeader />
-          <Routes>
-            <Route path="/testBear" element={<TestBear />} />
-          </Routes>
-        </BrowserRouter>
-      </LayoutBox>
-    </AppLayout>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppLayout>
+        <LayoutBox>
+          <BrowserRouter>
+            <HomeHeader />
+            <Routes>
+              <Route path="/testBear" element={<TestBear />} />
+            </Routes>
+          </BrowserRouter>
+        </LayoutBox>
+      </AppLayout>
+    </ThemeProvider>
   </div>
 );
 
